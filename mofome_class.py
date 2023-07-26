1e334ca8cc228e53597ca108b953eb66827c7fd5
Arthur Vinicius <111816515+arthurvfr@users.noreply.github.com>
2023-07-26 12:05:06 -0300

    Add files via upload

---
+++ mofome_class.py
@@ -0,0 +1,27 @@
+class Account():
+    def __init__(self, name, password, username):
+        #self.name = name
+        #self.password = password
+        #self.username = username
+    
+    def show(self):
+        print("", self.name)
+        print("", self.password)
+        print("", self.username)
+
+    def AccountManagement(self, name, password, username):
+
+class Receitas():
+    def __init__(self, numero_da_receita, nome, ingredientes, modo_de_preparo, avaliacoes, excluir_receita)
+
+class Topicos():
+    def __init__(self, numero_do_topico, contribuidores, excluir_topico)
+
+class Comentarios():
+    def __init__(self, autor, texto, data_e_hora)
+
+class Sistema():
+    def __init__(self, lista_de_receitas, lista_de_usuarios, lista_de_topicos)
+
+class Privacidade():
+    def __init__(self, modo_privacidade)
